0b96e41435bf30a0b7922be87ed24e342ac0d0a4
abdalodj <lodianea98@gmail.com>
2021-10-21 12:48:38 +0000

    Lesson P2C2 end

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,12 @@
+import '../styles/Banner.css'
+import logo from "../assets/logo.png";
 
-const Banner = () => <h1>La maison de la jungle</h1>;
+const title = "La maison de la jungle";
+const Banner = () => (
+    <div className="lmj-banner" /*style={{textTransform: "uppercase"}}*/>
+        <img src={logo} alt="La maison de la jungle" className="lmj-logo"/>
+        <h1>{title}</h1>
+    </div>
+);
 
 export default Banner;
