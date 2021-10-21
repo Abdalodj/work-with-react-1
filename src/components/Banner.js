d39f67264e8708f360518ba90458861ef53ed7b2
abdalodj <lodianea98@gmail.com>
2021-10-21 21:06:22 +0000

    Lesson P2C5

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,5 @@
 import '../styles/Banner.css'
-import logo from "../assets/logo.png";
 
-const title = "La maison de la jungle";
 const Banner = ({children}) => <div className='lmj-banner'>{children}</div>;
 
 export default Banner;
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,11 +1,13 @@
 import CareScale from "./CareScale";
 import "../styles/PlantItem.css";
 
-
+function handleClick(e) {
+    console.log('✨ Ceci est mon event :', e)
+}
 
 export function PlantItem({name, cover, id, light, water, isSpecialOffer}) {
     return (
-        <li className='lmj-plant-item' key={id}>
+        <li className='lmj-plant-item' key={id} onClick={handleClick}>
             <img src={cover} alt={`${name} cover`} className="lmj-plant-item-cover"/>
             {name}
             {
